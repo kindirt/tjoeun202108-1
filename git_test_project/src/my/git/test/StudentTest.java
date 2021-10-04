dc76a2d5207b2f91c09f185ec6976004424e1463
HP <HP@DESKTOP-R997P7V>
2021-10-04 17:39:51 +0900

    안녕하세요 출력 추가

--- git_test_project/src/my/git/test/StudentTest.java
+++ git_test_project/src/my/git/test/StudentTest.java
@@ -2,16 +2,18 @@ package my.git.test;
 
 public class StudentTest {
 	public static void main(String[] args) {
-		//여기서 소스를 수정하면 git의 워크트리에 있는 파일이 수정되는 것임!!
-		//워크트리   ->   인덱스   ->   로컬저장소   ->   원격저장소
+		//�뿬湲곗꽌 �냼�뒪瑜� �닔�젙�븯硫� git�쓽 �썙�겕�듃由ъ뿉 �엳�뒗 �뙆�씪�씠 �닔�젙�릺�뒗 寃껋엫!!
+		//�썙�겕�듃由�   ->   �씤�뜳�뒪   ->   濡쒖뺄���옣�냼   ->   �썝寃⑹��옣�냼
 		//    "add index    "commit"         "put"  
 		
-		Student st = new Student("하하",40);
+		Student st = new Student("�븯�븯",40);
 		System.out.println(st + "!!!!");
 		System.out.println(st);
 		
-		Student st1 = new Student("하하하하",40);
+		Student st1 = new Student("�븯�븯�븯�븯",40);
 		System.out.println(st1 + "!!!!");
 		System.out.println(st1);
+		
+		System.out.println("안녕하세요");
 	}
 }
